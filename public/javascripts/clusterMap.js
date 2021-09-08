28749ea1ed2171ef07dc6c77c5f5307e6536ae89
Tanish chaurasia <59193918+skadoodle1201@users.noreply.github.com>
2021-09-08 08:29:25 +0530

    clustermap change

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'cluster-map',
     style: 'mapbox://styles/mapbox/light-v10',
-    center: [-103.59179687498357, 40.66995747013945],
+    center: [0.000, 0.000],
     zoom: 3
 });
 
